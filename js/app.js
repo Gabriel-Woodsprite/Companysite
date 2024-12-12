5cdb6cca8a69931d3f1c74e383c6959848e16add
Gabriel G. <L23650343@zitacuaro.tecnm.mx>
2024-12-12 11:38:07 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// const slider = document.querySelector(".slider");
+// const items = [...slider.children];
+
+// // Duplica los elementos si es necesario para garantizar un desplazamiento suave
+// while (slider.scrollWidth < slider.clientWidth * 2) {
+// 	items.forEach(item => {
+// 		const clone = item.cloneNode(true);
+// 		slider.appendChild(clone);
+// 	});
+// }
---
+++ js/indexBg.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", () => {
+// 	const indexBg = document.querySelector(".indexBg");
+// 	const gradient = document.createElement("div");
+// 	gradient.className = "gradient-follow";
+// 	indexBg.appendChild(gradient);
+
+// 	document.addEventListener("mousemove", event => {
+// 		const { clientX: x, clientY: y } = event;
+
+// 		// Adjust position of the gradient around the cursor
+// 		gradient.style.transform = `translate(${x}px, ${y}px)`;
+// 	});
+// });
